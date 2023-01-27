eb5995c7d57c756a539c02d4d4cc4ddac4bafb7c
nghianguyenbbv <nghia2.nguyen@bbv.vn>
2023-01-27 15:51:49 +0700

    update utilities unzip folder

--- FunnimartAutoDropship/src/main/java/selly/service/Utilities.java
+++ FunnimartAutoDropship/src/main/java/selly/service/Utilities.java
@@ -17,21 +17,21 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 public class Utilities {
-    public static void sleep(int sleepTime){
-        try{
+    public static void sleep(int sleepTime) {
+        try {
             Thread.sleep(sleepTime);
-        } catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("Error when sleeps");
         }
     }
 
-    public static void moveFolder(String folderName){
+    public static void moveFolder(String folderName) {
         File src = new File("D://funniMartDownload");
-        File dest = new File("D://funniMart//"+folderName+"");
+        File dest = new File("D://funniMart//" + folderName + "");
 
         try {
-            if(!dest.exists()){
+            if (!dest.exists()) {
                 FileUtils.moveDirectory(src, dest);
             }
 
@@ -40,55 +40,53 @@ public class Utilities {
         }
     }
 
-    public static void createNewFolder(String pathStr){
-        File path  = new File(pathStr);
-        if(!path.exists()){
-                 path.mkdirs();
+    public static void createNewFolder(String pathStr) {
+        File path = new File(pathStr);
+        if (!path.exists()) {
+            path.mkdirs();
 
         }
 
     }
 
-    public static  void createFileAndInsert(String content, String fileName){
+    public static void createFileAndInsert(String content, String fileName) {
         BufferedWriter buffredWrite = null;
-        try{
+        try {
 
             File newFile = new File(fileName);
-            if(!newFile.exists()){
+            if (!newFile.exists()) {
                 newFile.createNewFile();
             }
             FileWriter writeFile = new FileWriter(newFile);
             buffredWrite = new BufferedWriter(writeFile);
-            System.out.println("content"+content);
+            System.out.println("content" + content);
             buffredWrite.write(content);
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
-            System.out.println("error create file: "+ex);
-        }
-        finally
-        {
-            try{
-                if(buffredWrite!=null)
+            System.out.println("error create file: " + ex);
+        } finally {
+            try {
+                if (buffredWrite != null)
                     buffredWrite.close();
-            }catch(Exception ex){
-                System.out.println("Error in closing the BufferedWriter"+ex);
+            } catch (Exception ex) {
+                System.out.println("Error in closing the BufferedWriter" + ex);
             }
         }
     }
 
-    public static void gotoTopPage(WebDriver driver){
+    public static void gotoTopPage(WebDriver driver) {
         ((JavascriptExecutor) driver).executeScript("window.scrollTo(document.body.scrollHeight, 0)");
     }
 
-    public static void gotToEndPage(WebDriver driver){
+    public static void gotToEndPage(WebDriver driver) {
         ((JavascriptExecutor) driver).executeScript("window.scrollTo(0,document.body.scrollHeight)");
     }
 
-    public static void clichByJS(WebDriver driver, WebElement element){
+    public static void clichByJS(WebDriver driver, WebElement element) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
     }
 
-    public static Path zipSlipProtect(ZipEntry zipEntry, Path targetDir) throws  IOException{
+    public static Path zipSlipProtect(ZipEntry zipEntry, Path targetDir) throws IOException {
         Path targetDirResolved = targetDir.resolve(zipEntry.getName());
         Path normalizePath = targetDirResolved.normalize();
         if (!normalizePath.startsWith(targetDir)) {
@@ -99,11 +97,11 @@ public class Utilities {
     }
 
     public static void main(String[] args) {
-        String source = "D:/funniMart/ao-khoac/funi-ao-khoac-du-2-trong-1-chuyen-doi-thanh-balo-chong-tham-nuoc-lakbl015.zip";
+        String source = "D:/funniMart/Áo sat nách nam dáng thể thao thoáng mát ASN001 - UC-D9F4-275/funi-ao-sat-nach-nam-dang-the-thao-thoang-mat-asn001.zip";
         String targetDir = "D:/funniMart/ao-khoac";
         try {
-            unzip(source, targetDir);
-        }catch(Exception ex){
+            unzipFolder(source, targetDir);
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
@@ -122,21 +120,22 @@ public class Utilities {
             fis = new FileInputStream(zipFilePath);
             ZipInputStream zis = new ZipInputStream(fis);
             ZipEntry ze = zis.getNextEntry();
-            while (ze != null ) {
+            while (ze != null) {
                 //String updateDir
-                if(!ze.getName().contains("jpg")){
-                    new File(destDir+"/"+ze.getName()).mkdirs();
+                if (!ze.getName().contains("jpg")) {
+                    new File(destDir + "/" + ze.getName()).mkdirs();
                     //destDir = destDir+"/"+ze.getName();
-                }else{
+                } else {
                     String fileName = ze.getName();
                     File newFile = new File(destDir + File.separator + fileName);
                     System.out.println("Unzipping to " + newFile.getAbsolutePath());
                     //create directories for sub directories in zip
                     File parentFile = new File(newFile.getParent());
-                    if (!parentFile.exists())
-                    { parentFile.mkdirs();
+                    if (!parentFile.exists()) {
+                        parentFile.mkdirs();
                         //new File(newFile.getParent()).mkdirs();
-                        System.out.println("newFile.getParent(): "+newFile.getParent());}
+                        System.out.println("newFile.getParent(): " + newFile.getParent());
+                    }
                     FileOutputStream fos = new FileOutputStream(newFile);
                     int len;
                     while ((len = zis.read(buffer)) > 0) {
@@ -157,22 +156,25 @@ public class Utilities {
             e.printStackTrace();
         }
     }
-public static void unzip(InputStream iS, Path targetPath) throws IOException {
+
+    public static void unzipFolder(String source, String targetFolder) throws IOException {
+        Path targetPath = Paths.get(targetFolder) ;
+        InputStream iS = new FileInputStream(source);
         targetPath = targetPath.toAbsolutePath();
-        try(ZipInputStream zipStream = new ZipInputStream(iS)) {
+        try (ZipInputStream zipStream = new ZipInputStream(iS)) {
             for (ZipEntry ze; (ze = zipStream.getNextEntry()) != null; ) {
                 Path resolvedPath = targetPath.resolve(ze.getName()).normalize();
                 if (!resolvedPath.startsWith(targetPath)) {
                     throw new RuntimeException("Entry is invalid path: " + ze.getName());
                 }
-                if(ze.isDirectory()){
+                if (ze.isDirectory()) {
                     Files.createDirectories(resolvedPath);
-                }else{
+                } else {
                     Files.createDirectories(resolvedPath.getParent());
                     Files.copy(zipStream, resolvedPath);
                 }
             }
         }
-}
+    }
 
 }
